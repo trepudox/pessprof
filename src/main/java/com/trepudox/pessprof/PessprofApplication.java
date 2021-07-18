e8390fdec8edd8d6ceb2822f9f99c38477b069c5
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-18 20:06:21 -0300

    commit inicial

---
+++ src/main/java/com/trepudox/pessprof/PessprofApplication.java
@@ -0,0 +1,13 @@
+package com.trepudox.pessprof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessprofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessprofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trepudox/pessprof/PessprofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trepudox.pessprof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessprofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
