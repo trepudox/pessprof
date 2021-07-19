cde9171951003004ce99585e58ad543bc4bea867
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-19 00:33:46 -0300

    controllers 'funcionais'

--- src/main/java/com/trepudox/pessprof/controller/PessoaController.java
+++ src/main/java/com/trepudox/pessprof/controller/PessoaController.java
@@ -1,15 +1,33 @@
 package com.trepudox.pessprof.controller;
 
-import com.trepudox.pessprof.service.PessoaService;
+import com.trepudox.pessprof.model.Pessoa;
+import com.trepudox.pessprof.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController {
 
     @Autowired
-    private PessoaService pessoaService;
+//    private PessoaService pessoaService;
+    private PessoaRepository pessoaRepository;
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<Pessoa>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(pessoaRepository.findAll());
+    }
+
+    @GetMapping(path = "/id/{id}")
+    public ResponseEntity<Pessoa> getById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(pessoaRepository.findById(id).get());
+    }
 
 }
--- src/main/java/com/trepudox/pessprof/controller/ProfissaoController.java
+++ src/main/java/com/trepudox/pessprof/controller/ProfissaoController.java
@@ -1,15 +1,34 @@
 package com.trepudox.pessprof.controller;
 
+import com.trepudox.pessprof.model.Profissao;
+import com.trepudox.pessprof.repository.ProfissaoRepository;
 import com.trepudox.pessprof.service.ProfissaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/profissoes")
 public class ProfissaoController {
 
     @Autowired
-    private ProfissaoService profissaoService;
+//    private ProfissaoService profissaoService;
+    private ProfissaoRepository profissaoRepository;
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<Profissao>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(profissaoRepository.findAll());
+    }
+
+    @GetMapping(path = "/id/{id}")
+    public ResponseEntity<Profissao> getById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(profissaoRepository.findById(id).get());
+    }
 
 }
