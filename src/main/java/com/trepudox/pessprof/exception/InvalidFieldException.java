3f0dc57627d7943a5cb98755a547a2c36661a023
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-19 14:52:42 -0300

    criando nova exception

---
+++ src/main/java/com/trepudox/pessprof/exception/InvalidFieldException.java
@@ -0,0 +1,9 @@
+package com.trepudox.pessprof.exception;
+
+public class InvalidFieldException extends RuntimeException {
+
+    public InvalidFieldException(String message) {
+        super(message);
+    }
+
+}
