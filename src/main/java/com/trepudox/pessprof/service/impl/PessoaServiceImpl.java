10f122dd8ae576a0974b14eeabab23a72d6a0ee1
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-19 00:34:36 -0300

    adicionando metodos na implementacao

--- src/main/java/com/trepudox/pessprof/service/impl/PessoaServiceImpl.java
+++ src/main/java/com/trepudox/pessprof/service/impl/PessoaServiceImpl.java
@@ -1,8 +1,47 @@
 package com.trepudox.pessprof.service.impl;
 
+import com.trepudox.pessprof.model.Pessoa;
+import com.trepudox.pessprof.model.Profissao;
+import com.trepudox.pessprof.repository.PessoaRepository;
 import com.trepudox.pessprof.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PessoaServiceImpl implements PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Override
+    public Pessoa findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Pessoa> findAllByNome(String nome) {
+        return null;
+    }
+
+    @Override
+    public List<Profissao> findAll() {
+        return null;
+    }
+
+    @Override
+    public Pessoa create(Pessoa pessoa) {
+        return null;
+    }
+
+    @Override
+    public Pessoa update(Pessoa pessoa) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
--- src/main/java/com/trepudox/pessprof/service/impl/ProfissaoServiceImpl.java
+++ src/main/java/com/trepudox/pessprof/service/impl/ProfissaoServiceImpl.java
@@ -1,8 +1,46 @@
 package com.trepudox.pessprof.service.impl;
 
+import com.trepudox.pessprof.model.Profissao;
+import com.trepudox.pessprof.repository.ProfissaoRepository;
 import com.trepudox.pessprof.service.ProfissaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProfissaoServiceImpl implements ProfissaoService {
+
+    @Autowired
+    private ProfissaoRepository profissaoRepository;
+
+    @Override
+    public Profissao findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Profissao> findAllByNome(String nome) {
+        return null;
+    }
+
+    @Override
+    public List<Profissao> findAll() {
+        return null;
+    }
+
+    @Override
+    public Profissao create(Profissao profissao) {
+        return null;
+    }
+
+    @Override
+    public Profissao update(Profissao profissao) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
