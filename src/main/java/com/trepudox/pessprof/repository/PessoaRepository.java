d04c26807057967ac7b5ce5d331d66fe1b473722
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-19 00:34:18 -0300

    adicionando metodo novo

--- src/main/java/com/trepudox/pessprof/repository/PessoaRepository.java
+++ src/main/java/com/trepudox/pessprof/repository/PessoaRepository.java
@@ -4,6 +4,9 @@ import com.trepudox.pessprof.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    List<Pessoa> findAllByNomeContainingIgnoreCase(String nome);
 }
--- src/main/java/com/trepudox/pessprof/repository/ProfissaoRepository.java
+++ src/main/java/com/trepudox/pessprof/repository/ProfissaoRepository.java
@@ -4,6 +4,9 @@ import com.trepudox.pessprof.model.Profissao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProfissaoRepository extends JpaRepository<Profissao, Long> {
+    List<Profissao> findAllByNomeContainingIgnoreCase(String nome);
 }
