9d68fa38ae1de536a3d322c515148d3465da6885
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-20 10:59:13 -0300

    excluindo variavel desnecessaria

--- src/main/java/com/trepudox/pessprof/exception/APIExceptionHandler.java
+++ src/main/java/com/trepudox/pessprof/exception/APIExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
-
 @RestControllerAdvice
 public class APIExceptionHandler {
 
