ec122dad12f8eaf8d9f330ae4777a39004f4786b
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-21 17:20:31 -0300

    metodos implementados

--- src/main/java/com/trepudox/pessprof/service/impl/ProfissaoServiceImpl.java
+++ src/main/java/com/trepudox/pessprof/service/impl/ProfissaoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.trepudox.pessprof.service.impl;
 
+import com.trepudox.pessprof.exception.InvalidFieldException;
 import com.trepudox.pessprof.exception.profissao.ProfissaoNotFoundException;
 import com.trepudox.pessprof.model.Profissao;
 import com.trepudox.pessprof.repository.ProfissaoRepository;
@@ -17,12 +18,22 @@ public class ProfissaoServiceImpl implements ProfissaoService {
 
     @Override
     public List<Profissao> findAll() {
-        return null;
+        List<Profissao> lista = profissaoRepository.findAll();
+
+        if (lista.isEmpty())
+            throw new ProfissaoNotFoundException("Nenhuma profissão foi encontrada.");
+
+        return lista;
     }
 
     @Override
     public List<Profissao> findAllByNome(String nome) {
-        return null;
+        List<Profissao> lista = profissaoRepository.findAllByNomeContainingIgnoreCase(nome);
+
+        if (lista.isEmpty())
+            throw new ProfissaoNotFoundException("Nenhuma profissão foi encontrada com esse nome.");
+
+        return lista;
     }
 
     @Override
@@ -33,16 +44,45 @@ public class ProfissaoServiceImpl implements ProfissaoService {
 
     @Override
     public Profissao create(Profissao profissao) {
-        return null;
+        Profissao profissaoFinal = validaCampos(profissao);
+
+        profissaoFinal.setId(0L);
+        profissaoFinal.setPessoas(List.of());
+
+        return profissaoRepository.save(profissaoFinal);
     }
 
     @Override
     public Profissao update(Profissao profissao) {
-        return null;
+        Profissao profissaoProcurada = findById(profissao.getId());
+
+        Profissao profissaoFinal = validaCampos(profissaoProcurada);
+
+        profissaoFinal.setId(profissaoProcurada.getId());
+
+        return profissaoRepository.save(profissaoFinal);
     }
 
     @Override
     public void deleteById(Long id) {
+        Profissao profissao = findById(id);
 
+        profissaoRepository.delete(profissao);
+    }
+
+    private static Profissao validaCampos(Profissao profissao) {
+        String nome = profissao.getNome();
+        Double salario = profissao.getSalario();
+
+        boolean nomeInvalido = nome == null || nome.isBlank();
+        boolean salarioInvalido = salario == null || salario < 0;
+
+        if (nomeInvalido)
+            throw new InvalidFieldException("Nome não pode estar vazio!");
+
+        if (salarioInvalido)
+            throw new InvalidFieldException("Salário não pode estar vazio e tem que ser maior ou igual a 0!");
+
+        return new Profissao(null, nome, salario, null);
     }
 }
